6587b55b54091683f19abab47462765d7878a264
s.rezenkov <s.rezenkov@metamodel.ru>
2024-09-11 22:13:11 +0500

    task 'Book'

---
+++ src/main/java/org/example/homework/tasks/book/Book.java
@@ -0,0 +1,40 @@
+package org.example.homework.tasks.book;
+
+public class Book implements Displayable{
+    private String name;
+    private String author;
+    private String yearOfPublication;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getYearOfPublication() {
+        return yearOfPublication;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setYearOfPublication(String yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+    }
+
+    @Override
+    public void display() {
+        String bookInfo = String.format("This is the book '%s' by %s, %s edition",
+                this.name,
+                this.author,
+                this.yearOfPublication);
+        System.out.println(bookInfo);
+    }
+}
---
+++ src/main/java/org/example/homework/tasks/book/Displayable.java
@@ -0,0 +1,6 @@
+package org.example.homework.tasks.book;
+
+public interface Displayable {
+
+    void display();
+}
